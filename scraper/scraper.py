d1253ff6e848c8461ec7a975cb68329324e2f978
RobFed <47362512+RobFed@users.noreply.github.com>
2021-04-11 15:07:18 -0400

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import calendar 
+import sys
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 
@@ -11,14 +12,28 @@ courses = []
 
 ts = calendar.timegm(time.gmtime())
 
+#term = sys.argv
+
+#temp input until I know what system arguments to look for
+term = input("enter term: ")
+
+
 for i in range(len(courseCodes)):
     for x in range(8):     
 
         # TODO: make the term dynamic from a commandline argument
-        response = requests.get("https://scheduleme.wlu.ca/vsb/add_suggest.jsp?term=202105&cams=C_K_T_V_W_Z_CC_G_X_Y_MC&course_add=" +
+        #202105
+        response = requests.get("https://scheduleme.wlu.ca/vsb/add_suggest.jsp?term=" + str(term) +"&cams=C_K_T_V_W_Z_CC_G_X_Y_MC&course_add=" +
                                 courseCodes[i] + "&page_num=" + str(x) + "&_=" + str(ts))
         soup = BeautifulSoup(response.content, 'html.parser')
         #numCourses = soup.find('add_suggest', results="")
+
+        term_one = soup.find(id="term_202005")
+        term_two = soup.find(id="term_202009")
+        term_three = soup.find(id="term_202101")
+        term_four = soup.find(id="term_202105")
+
+        #print(term_one, term_two, term_three, term_four)
         # print(numCourses)
         for y in range(7):
             try:
