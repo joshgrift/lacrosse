e046ecdfe266870c8607430f6d7c5a91bf4d9403
Josh <josh@grift.ca>
2021-04-13 13:54:08 -0400

    Refactor to make work with new structure

--- lacrosse/urls.py
+++ lacrosse/urls.py
@@ -1,4 +1,4 @@
-"""djangoLACROSSE URL Configuration
+"""lacrosse URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from . import queries
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-urlpatterns = [
-    path('', include('Lacrosse.Lacrosse.urls')),
+    path("query", queries.query),
+    path("search-params", queries.searchParams),
+    path("*", views.hello_world),
+    path('', views.hello_world)
 ]
